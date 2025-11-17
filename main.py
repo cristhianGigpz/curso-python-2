6540f4e35016d51f893d46ba10b6fc8e4f72cfaa
cristhianGigpz <cjacevedot@gmail.com>
2025-11-17 14:45:23 -0500

    first commit pep8 curso intermedio python

---
+++ main.py
@@ -0,0 +1,18 @@
+# Longitud de linea: maximo 79 caracteres (Ruff default)
+# Identacion: 4 espacios por nivel, nunca tabs
+# Nombres descriptivos: snake_case para funciones y variables, CamelCase para clases
+# Imports ordenados: estandar, terceros, locales
+# Comentarios: Explicativos y en ingles, no obvios
+# Comillas consitentes: dobles para strings
+
+API_KEY = "your_api_key_here"
+DEFAULT_TIMEOUT = 30  # seconds
+DEFAULT_LANGUAGE = "en"
+
+def process_article_cata(data):
+    """obtiene noticas desde la API especifica"""
+
+
+def fetch_news_from_api(api_key, timeout=DEFAULT_TIMEOUT, language=DEFAULT_LANGUAGE):
+    """Fetch news articles from the specified API."""
+    pass
